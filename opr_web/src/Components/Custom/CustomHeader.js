64784d5954538257dc3d3bcd9ce5b96f39bd0f97
Sashi Kumar Reddy <sashikumar.reddy@smartgig.tech>
2024-06-19 15:16:35 +0530

    Api

--- opr_web/src/Components/Custom/CustomHeader.js
+++ opr_web/src/Components/Custom/CustomHeader.js
@@ -1,85 +1,96 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import "./Custom.css";
 import axiosInstance from "../../api/axios";
 
 const CustomHeader = () => {
-  const [selectValue1, setSelectValue1] = useState(null);
-  const [selectValue2, setSelectValue2] = useState(null);
-  const [selectValue3, setSelectValue3] = useState(null);
-  const [selectValue4, setSelectValue4] = useState(null);
-
-  const options1 = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-  ];
+  const [selectHo, setSelectHo] = useState(null);
+  const [selectRo, setSelectRo] = useState(null);
+  const [selectBank, setSelectBank] = useState(null);
+  const [selectBranch, setSelectBranch] = useState(null);
+  
+  const [hoOption, setHoOption] = useState([]);
+  const [roOptions, setRoOptions] = useState([]);
+  const [bankOptions, setBankOptions] = useState([]);
+  const [branchOptions, setBranchOptions] = useState([]);
 
   useEffect(() => {
     const fetchVisibleEntities = async () => {
       try {
-        const response = await axiosInstance.post("/common-utils/call-stored-procedure",{
+        const response = await axiosInstance.post("/common-utils/call-stored-procedure", {
           "procedure": "get_visible_entities",
           "level": 0,
           "incSelf": true
         });
-        console.log(response.data?.data);
+        const data = response.data?.data?.data;
+        if (data) {
+          const grouped = data.reduce((acc, unit) => {
+            const { unitLevel } = unit;
+            if (!acc[unitLevel]) {
+              acc[unitLevel] = [];
+            }
+            acc[unitLevel].push({ value: unit.unitCode, label: unit.unitName });
+            return acc;
+          }, {});
+          setHoOption(grouped[1] || []);
+          setRoOptions(grouped[2] || []);
+          setBankOptions(grouped[3] || []);
+          setBranchOptions(grouped[4] || []);
+        }
       } catch (error) {
         console.log("Error fetching config:", error);
       }
     };
     fetchVisibleEntities();
   }, []);
-  
 
   return (
-
-      <div className="select-container">
-        <div className="select-wrapper">
-          <label className="select-label">Head Office</label>
-          <Select
-            value={selectValue1}
-            onChange={setSelectValue1}
-            options={options1}
-            placeholder="Select option 1"
-            className="custom-select-container"
-            classNamePrefix="custom-select"
-          />
-        </div>
-        <div className="select-wrapper">
-          <label className="select-label">Regional Office</label>
-          <Select
-            value={selectValue2}
-            onChange={setSelectValue2}
-            options={options1}
-            placeholder="Select option 2"
-            className="custom-select-container"
-            classNamePrefix="custom-select"
-          />
-        </div>
-        <div className="select-wrapper">
-          <label className="select-label">Banks</label>
-          <Select
-            value={selectValue3}
-            onChange={setSelectValue3}
-            options={options1}
-            placeholder="Select option 3"
-            className="custom-select-container"
-            classNamePrefix="custom-select"
-          />
-        </div>
-        <div className="select-wrapper">
-          <label className="select-label">Branches</label>
-          <Select
-            value={selectValue4}
-            onChange={setSelectValue4}
-            options={options1}
-            placeholder="Select option 4"
-            className="custom-select-container"
-            classNamePrefix="custom-select"
-          />
-        </div>
+    <div className="select-container">
+      <div className="select-wrapper">
+        <label className="select-label">Head Office</label>
+        <Select
+          value={selectHo}
+          onChange={setSelectHo}
+          options={hoOption}
+          placeholder="Select HO"
+          className="custom-select-container"
+          classNamePrefix="custom-select"
+        />
       </div>
-
+      <div className="select-wrapper">
+        <label className="select-label">Regional Office</label>
+        <Select
+          value={selectRo}
+          onChange={setSelectRo}
+          options={roOptions}
+          placeholder="Select RO"
+          className="custom-select-container"
+          classNamePrefix="custom-select"
+        />
+      </div>
+      <div className="select-wrapper">
+        <label className="select-label">Banks</label>
+        <Select
+          value={selectBank}
+          onChange={setSelectBank}
+          options={bankOptions}
+          placeholder="Select Bank"
+          className="custom-select-container"
+          classNamePrefix="custom-select"
+        />
+      </div>
+      <div className="select-wrapper">
+        <label className="select-label">Branches</label>
+        <Select
+          value={selectBranch}
+          onChange={setSelectBranch}
+          options={branchOptions}
+          placeholder="Select Branch"
+          className="custom-select-container"
+          classNamePrefix="custom-select"
+        />
+      </div>
+    </div>
   );
 };
 
